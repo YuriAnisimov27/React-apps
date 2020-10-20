b5114daad19fb1853a64b0dd4fa1824552112ee1
YuriAnisimov27 <yurianisimov27@gmail.com>
2020-10-21 02:19:03 +0300

    feat: added application file structure

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,38 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
-import * as serviceWorker from './serviceWorker';
+import App from "./components/app";
 
-const Header = () => {
-  return <h1 style={{'color': 'red'}}>Hello</h1>
-}
-
-const Field = () => {
-  return <input type="text" placeholder='Type here'/>
-}
-
-const Btn = () => {
-  const text = 'Log in'
-  return <button>{text}</button>
-}
-
-const App = () => {
-  return (
-    <div>
-      <Header />
-      <Field />
-      <Btn />
-    </div>
-  )
-}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App/>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-
-serviceWorker.unregister();
