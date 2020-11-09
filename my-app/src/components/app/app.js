0b019402b7f9cdab1d5d7e4f4b90cb64ef5e5100
YuriAnisimov27 <yurianisimov27@gmail.com>
2020-11-09 22:23:54 +0300

    feat: implement option to delete item from list

--- my-app/src/components/app/app.js
+++ my-app/src/components/app/app.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import AppHeader from "../app-header";
 import SearchPanel from "../search-panel";
 import PostStatusFilter from "../post-status-filter";
@@ -6,37 +6,53 @@ import PostList from "../post-list";
 import PostAddForm from "../post-add-form";
 import './app.css';
 
-const App = () => {
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [
+        {
+          label: 'Don\'t Worry, Be Happy)',
+          important: true,
+          id: 1
+        },
+        {
+          label: 'Greed Is Good',
+          important: false,
+          id: 2
+        },
+        {
+          label: 'Terminator 2',
+          important: false,
+          id: 3
+        }
+      ]
+    };
+    this.deleteItem = this.deleteItem.bind(this)
+  }
 
-  const data = [
-    {
-      label: 'Don\'t Worry, Be Happy)',
-      important: true,
-      id: 1
-    },
-    {
-      label: 'Greed Is Good',
-      important: false,
-      id: 2
-    },
-    {
-      label: 'Terminator 2',
-      important: false,
-      id: 3
-    }
-  ];
+  deleteItem(id) {
+    this.setState(({data}) => {
+      const index = data.findIndex(elem => elem.id === id)
+      const newArr = [...data.slice(0, index), ...data.slice(index + 1)]
 
-  return (
-    <div className='app'>
-      <AppHeader/>
-      <div className='search-panel d-flex'>
-        <SearchPanel/>
-        <PostStatusFilter/>
+      return {
+        data: newArr
+      }
+    })
+  }
+
+  render() {
+    return (
+      <div className='app'>
+        <AppHeader/>
+        <div className='search-panel d-flex'>
+          <SearchPanel/>
+          <PostStatusFilter/>
+        </div>
+        <PostList posts={this.state.data} onDelete={this.deleteItem}/>
+        <PostAddForm/>
       </div>
-      <PostList posts={data}/>
-      <PostAddForm/>
-    </div>
-  )
+    )
+  }
 }
-
-export default App;
