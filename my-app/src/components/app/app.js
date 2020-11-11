6414ca7d0828912488f94355883ea2debaee1394
YuriAnisimov27 <yurianisimov27@gmail.com>
2020-11-11 18:37:19 +0300

    feat: add posts counter in header

--- my-app/src/components/app/app.js
+++ my-app/src/components/app/app.js
@@ -14,21 +14,29 @@ export default class App extends Component {
         {
           label: 'Don\'t Worry, Be Happy)',
           important: true,
-          id: 1
+          like: false,
+          id: 1,
         },
         {
           label: 'Greed Is Good',
           important: false,
-          id: 2
+          like: false,
+          id: 2,
         },
         {
           label: 'Terminator 2',
           important: false,
-          id: 3
+          like: false,
+          id: 3,
         }
       ]
     };
-    this.deleteItem = this.deleteItem.bind(this)
+    this.deleteItem = this.deleteItem.bind(this);
+    this.addItem = this.addItem.bind(this);
+    this.onToggleImportant = this.onToggleImportant.bind(this);
+    this.onToggleLiked = this.onToggleLiked.bind(this);
+
+    this.maxId = 4;
   }
 
   deleteItem(id) {
@@ -42,16 +50,60 @@ export default class App extends Component {
     })
   }
 
+  addItem(body) {
+    const newItem = {
+      label: body,
+      important: false,
+      id: this.maxId++
+    }
+
+    this.setState(({data}) => {
+      const newArr = [...data, newItem]
+      return {
+        data: newArr
+      }
+    })
+  }
+
+  toggleAttribute(id, attr) {
+    this.setState(({data}) => {
+      const index = data.findIndex(elem => elem.id === id)
+      const old = data[index];
+      const newItem = {...old, [attr]: !old[attr]}
+      const newArr = [...data.slice(0, index), newItem, ...data.slice(index + 1)]
+
+      return {
+        data: newArr
+      }
+    })
+  }
+
+  onToggleImportant(id) {
+    this.toggleAttribute(id, 'important')
+  }
+
+  onToggleLiked(id) {
+    this.toggleAttribute(id, 'like')
+  }
+
   render() {
+    const {data} = this.state
+    const liked = data.filter(item => item.like).length
+    const allPosts = data.length
+
     return (
       <div className='app'>
-        <AppHeader/>
+        <AppHeader liked={liked} allPosts={allPosts}/>
         <div className='search-panel d-flex'>
           <SearchPanel/>
           <PostStatusFilter/>
         </div>
-        <PostList posts={this.state.data} onDelete={this.deleteItem}/>
-        <PostAddForm/>
+        <PostList
+          posts={this.state.data}
+          onDelete={this.deleteItem}
+          onToggleImportant={this.onToggleImportant}
+          onToggleLiked={this.onToggleLiked}/>
+        <PostAddForm onAdd={this.addItem}/>
       </div>
     )
   }
--- my-app/src/components/post-list/post-list.js
+++ my-app/src/components/post-list/post-list.js
@@ -2,14 +2,16 @@ import React from 'react';
 import './post-list.css';
 import PostListItem from "../post-list-item";
 
-const PostList = ({posts, onDelete}) => {
+const PostList = ({posts, onDelete, onToggleImportant,  onToggleLiked}) => {
 
   const elements = posts.map((item) => {
     const {id, ...itemProps} = item
 
     return (
       <li key={id} className='list-group-item'>
-        <PostListItem {...itemProps} onDelete={() => onDelete(id)} />
+        <PostListItem {...itemProps} onDelete={() => onDelete(id)}
+                      onToggleLiked={() => onToggleLiked(id)}
+                      onToggleImportant={() => onToggleImportant(id)} />
       </li>
     )
   })
