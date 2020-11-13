44c7156dc7d7f5aac9957bf276e52bedf2c79a10
YuriAnisimov27 <yurianisimov27@gmail.com>
2020-11-13 16:23:13 +0300

    feat: add header

--- react-material-ui/src/App.js
+++ react-material-ui/src/App.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react'
 import { Button } from '@material-ui/core'
+import {AppHeader} from './components/header';
 
 export default class App extends Component {
 
   render() {
     return (
-      <div className="App" style={{padding: 100}}>
+      <div className="App">
+        <AppHeader />
         still working...
         <Button color="primary">Click Me))</Button>
       </div>
