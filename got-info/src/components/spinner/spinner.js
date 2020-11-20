cf01e4e818d392acd600b72d25671ee79fc1d3c9
Yuri Anisimov <yurianisimov27@gmail.com>
2020-11-20 16:21:34 +0300

    feat: add error & spinner components

---
+++ got-info/src/components/spinner/spinner.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './spinner.css';
+
+const Spinner = () => {
+  return (
+    <div className="loadingio-spinner-bean-eater-ofirw76w07n">
+      <div className="ldio-6we8iglj4nv">
+        <div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+
+  );
+};
+
+export default Spinner;
