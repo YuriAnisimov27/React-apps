e353459e5bb76e9b81794c8183d93d3931865966
YuriAnisimov27 <yurianisimov27@gmail.com>
2020-12-25 13:08:06 +0300

    feat: add listener onClick

--- todo/src/components/todo-list-item/todo-list-item.js
+++ todo/src/components/todo-list-item/todo-list-item.js
@@ -2,6 +2,10 @@ import React, {Component} from 'react';
 import './todo-list-item.css';
 
 export default class TodoListItem extends Component {
+  onLabelClick = () => {
+    console.log(`click on ${this.props.label}`);
+  }
+
   render() {
     const {label, important = false} = this.props;
     const style = {
@@ -13,7 +17,8 @@ export default class TodoListItem extends Component {
       <span className="todo-list-item">
       <span
         className="todo-list-item-label"
-        style={style}>
+        style={style}
+        onClick={this.onLabelClick}>
         {label}
       </span>
 
