2ca6ede996acaed08d8953da4299bf669abe1dc0
YuriAnisimov27 <yurianisimov27@gmail.com>
2021-01-09 18:34:11 +0300

    feat:L add API-service

--- star-db/src/index.js
+++ star-db/src/index.js
@@ -1,11 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+class SwapiService {
 
-console.log('hello world')
+  _apiBase = 'https://swapi.dev/api';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <h1>Hello Star DB</h1>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  async getResource(url) {
+    const res = await fetch(`${this._apiBase}${url}`);
+    if (!res.ok) {
+      throw new Error(`Could Not Fetch, url: ${url}`);
+    }
+    return await res.json();
+  }
+
+  async getAllPeople() {
+    const res = await this.getResource(`/people/`);
+    return res.results;
+  }
+
+  getPerson(id) {
+    return this.getResource(`/people/${id}`);
+  }
+
+  async getAllPlanets() {
+    const res = await this.getResource(`/planets/`);
+    return res.results;
+  }
+
+  getPlanet(id) {
+    return this.getResource(`/planets/${id}`);
+  }
+
+  async getAllStarships() {
+    const res = await this.getResource(`/starships/`);
+    return res.results;
+  }
+
+  getStarship(id) {
+    return this.getResource(`/starships/${id}`);
+  }
+}
