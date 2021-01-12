74c065e60674f0cd59314d7863001ba6ed8b3fda
YuriAnisimov27 <yurianisimov27@gmail.com>
2021-01-12 15:10:02 +0300

    feat: add component spinner

--- star-db/src/components/app/app.js
+++ star-db/src/components/app/app.js
@@ -1,24 +1,22 @@
 import React from 'react';
-
 import Header from '../header';
 import RandomPlanet from '../random-planet';
 import ItemList from '../item-list';
 import PersonDetails from '../person-details';
-
 import './app.css';
 
 const App = () => {
   return (
     <div>
-      <Header />
-      <RandomPlanet />
+      <Header/>
+      <RandomPlanet/>
 
       <div className="row mb2">
         <div className="col-md-6">
-          <ItemList />
+          <ItemList/>
         </div>
         <div className="col-md-6">
-          <PersonDetails />
+          <PersonDetails/>
         </div>
       </div>
     </div>
---
+++ star-db/src/components/spinner/spiner.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './spiner.css';
+
+function Spinner() {
+  return (
+    <div className="d-flex-spinner">
+      <div className="loadingio-spinner-double-ring-8mvwvls3ewl">
+        <div className="ldio-2cyg1tsuhq9">
+          <div></div>
+          <div></div>
+          <div>
+            <div></div>
+          </div>
+          <div>
+            <div></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Spinner;
