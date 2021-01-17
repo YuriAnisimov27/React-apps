e52aa5770b64d6f6233c19f8ea68f25fb344805c
YuriAnisimov27 <yurianisimov27@gmail.com>
2021-01-18 02:24:07 +0300

    feat: add ErrorBoundry component

--- star-db/src/components/people-page/people-page.js
+++ star-db/src/components/people-page/people-page.js
@@ -1,34 +1,24 @@
 import React, {Component} from 'react';
 import ItemList from '../item-list/item-list';
 import PersonDetails from '../person-details/person-details';
-import ErrorIndicator from '../error-indicator/error-indicator';
 import SwapiService from '../../services/swapi-service';
 import Row from '../row';
 import './people-page.css';
+import ErrorBoundry from '../error-boundry';
+
 
 export default class PeoplePage extends Component {
   state = {
-    selectedPerson: Math.ceil(Math.random() * 83),
-    hasError: false
+    selectedPerson: Math.ceil(Math.random() * 83)
   };
 
   swapiService = new SwapiService();
-
-  componentDidCatch(error, info) {
-    this.setState({
-      hasError: true
-    });
-  }
 
   onPersonSelected = (selectedPerson) => {
     this.setState({selectedPerson});
   };
 
   render() {
-    if (this.state.hasError) {
-      return <ErrorIndicator/>;
-    }
-
     const itemList = (
       <ItemList
         onItemSelected={this.onPersonSelected}
@@ -37,7 +27,9 @@ export default class PeoplePage extends Component {
     );
 
     const personDetails = (
-      <PersonDetails personId={this.state.selectedPerson}/>
+      <ErrorBoundry>
+        <PersonDetails personId={this.state.selectedPerson}/>
+      </ErrorBoundry>
     );
 
     return (
