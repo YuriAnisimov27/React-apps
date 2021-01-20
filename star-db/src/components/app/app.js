ae80387c4c04ecf31a9abe744b4fb60af097f259
YuriAnisimov27 <yurianisimov27@gmail.com>
2021-01-21 00:25:03 +0300

    feat: HOC composition

--- star-db/src/components/app/app.js
+++ star-db/src/components/app/app.js
@@ -3,15 +3,10 @@ import Header from '../header';
 import RandomPlanet from '../random-planet';
 import ErrorButton from '../error-button';
 import ErrorIndicator from '../error-indicator';
-import PeoplePage from '../people-page';
-import ItemList from '../item-list';
 import SwapiService from '../../services/swapi-service';
-import Row from '../row';
-import ItemDetails from '../item-details';
-import {Record} from '../item-details/item-details';
 import './app.css';
 import ErrorBoundry from '../error-boundry';
-import {PersonDetails, PersonList, PlanetDetails, StarshipDetails, StarshipList} from '../sw-components';
+import {PersonDetails, PersonList, PlanetDetails, PlanetList, StarshipDetails, StarshipList} from '../sw-components';
 
 export default class App extends Component {
   state = {
@@ -38,26 +33,7 @@ export default class App extends Component {
       return <ErrorIndicator/>;
     }
 
-    const planet = this.state.showRandomPlanet ?
-      <RandomPlanet/> :
-      null;
-
-    const {getPerson, getStarship, getPersonImage, getStarshipImage, getAllPeople, getAllPlanets} = this.swapiService;
-
-    const personDetails = (
-      <ItemDetails itemId={11} getData={getPerson} getImageUrl={getPersonImage}>
-        <Record field='gender' label='Gender'/>
-        <Record field='eyeColor' label='Eye Color'/>
-      </ItemDetails>
-    );
-
-    const starShipDetails = (
-      <ItemDetails itemId={9} getData={getStarship} getImageUrl={getStarshipImage}>
-        <Record field='model' label='Model'/>
-        <Record field='length' label='Length'/>
-        <Record field='costInCredits' label='Cost'/>
-      </ItemDetails>
-    );
+    const planet = this.state.showRandomPlanet ? <RandomPlanet/> : null;
 
     return (
       <ErrorBoundry>
@@ -70,29 +46,15 @@ export default class App extends Component {
             style={{marginBottom: '30px'}}>
             Toggle Random Planet
           </button>
-
           <ErrorButton/>
 
           <PersonDetails itemId={1}/>
           <PlanetDetails itemId={5}/>
-          {/*<StarshipDetails itemId={7}/>*/}
+          <StarshipDetails itemId={5}/>
 
-          <PersonList>
-            {({name}) => <span>{name}</span>}
-          </PersonList>
-
-          <PersonList>
-            {({name}) => <span>{name}</span>}
-          </PersonList>
-
-          <StarshipList>
-            {({name}) => <span>{name}</span>}
-          </StarshipList>
-
-          {/*<ItemList getData={getAllPlanets}>*/}
-          {/*  {({name}) => <span>{name}</span>}*/}
-          {/*</ItemList>*/}
-
+          <PersonList/>
+          <PlanetList/>
+          <StarshipList/>
         </div>
       </ErrorBoundry>
     );
