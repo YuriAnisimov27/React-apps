e61a1f5db6fe6f299b83159bbb5af32cfa606b02
YuriAnisimov27 <yurianisimov27@gmail.com>
2021-01-29 17:52:18 +0300

    feat: add same logic to people page

--- star-db/src/components/pages/people-page.js
+++ star-db/src/components/pages/people-page.js
@@ -1,24 +1,14 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {PersonDetails, PersonList} from '../sw-components';
 import Row from '../row';
 
-export default class PeoplePage extends Component {
-  state = {
-    selectedItem: Math.ceil(Math.random() * 83)
-  };
+const PeoplePage = ({history}) => {
+  return (
+    <Row
+      left={<PersonList onItemSelected={(id) => history.push(id)}/>}
+      right={<PersonDetails itemId={Math.ceil(Math.random() * 83)}/>}
+    />
+  );
+};
 
-  onItemSelected = (selectedItem) => {
-    this.setState({selectedItem});
-  };
-
-  render() {
-    const {selectedItem} = this.state;
-
-    return (
-      <Row
-        left={<PersonList onItemSelected={this.onItemSelected}/>}
-        right={<PersonDetails itemId={selectedItem}/>}
-      />
-    );
-  }
-}
+export default PeoplePage;
--- star-db/src/components/pages/starships-page.js
+++ star-db/src/components/pages/starships-page.js
@@ -2,12 +2,6 @@ import React from 'react';
 import {StarshipList} from '../sw-components';
 import {withRouter} from 'react-router-dom';
 
-const StarshipsPage = ({history}) => {
-  return (
-    <StarshipList onItemSelected={(itemId) => {
-      history.push(`/starships/${itemId}`);
-    }}/>
-  );
-};
+const StarshipsPage = ({history}) => <StarshipList onItemSelected={(id) => history.push(id)}/>;
 
 export default withRouter(StarshipsPage);
