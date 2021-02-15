5d0cd92e7f4a41df6e89d9ae6a45188ccc95065a
YuriAnisimov27 <yurianisimov27@gmail.com>
2021-02-15 15:56:07 +0300

    feat: implement components sceleton in root index.js

--- resto/src/index.js
+++ resto/src/index.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router} from 'react-router-dom';
+import {Provider} from 'react-redux';
+import ErrorBoundry from './components/error-boundry';
+import RestoServiceContext from './components/resto-service-context';
+import RestoService from './services/resto-service';
 import App from './components/app';
-
+import store from './store';
 import './index.scss';
 
 
-ReactDOM.render(
-    <App/>
-    , document.getElementById('root'));
+const restoService = new RestoService();
 
+ReactDOM.render(
+  <Provider store={store}>
+    <ErrorBoundry>
+      <RestoServiceContext.Provider value={restoService}>
+        <Router>
+          <App/>
+        </Router>
+      </RestoServiceContext.Provider>
+    </ErrorBoundry>
+  </Provider>
+  , document.getElementById('root'));
