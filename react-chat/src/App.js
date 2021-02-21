6b2b0a5e21a9d084ca16b6b181b4d8355e9a5bcd
YuriAnisimov27 <yurianisimov27@gmail.com>
2021-02-21 17:09:20 +0300

    refactor: clean up project

--- react-chat/src/App.js
+++ react-chat/src/App.js
@@ -1,19 +1,7 @@
-import Welcome from './Welcome';
-import Clock from './Clock';
-import './App.css';
-import List from './List';
-
-
-const data = ['mary', 'john', 'bull', 'cat'];
-
 function App() {
   return (
     <div className="App">
-      <Welcome>
-        Vadik
-      </Welcome>
-      <Clock/>
-      <List data={data} />
+      <h1>Hello</h1>
     </div>
   );
 }
--- react-chat/src/List.js
+++
@@ -1,9 +0,0 @@
-const List = (props) => {
-  return (
-    <ol>
-      {props.data.map((el, idx) => <li key={idx}>{el}</li>)}
-    </ol>
-  )
-};
-
-export default List;
