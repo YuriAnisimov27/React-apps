2aabef94f6826ea3ee5898c6c92684d6aa3bd5af
YuriAnisimov27 <yurianisimov27@gmail.com>
2021-02-24 15:20:54 +0300

    feat: add axios request in useEffect

--- medium-clone/src/pages/authentication/index.js
+++ medium-clone/src/pages/authentication/index.js
@@ -1,7 +1,40 @@
+import {useState, useEffect} from 'react';
+import axios from 'axios';
 import {Link} from 'react-router-dom';
 
 
 const Authentication = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  useEffect(() => {
+    if (!isSubmitting) {
+      return;
+    }
+
+    axios('https://conduit.productionready.io/api/users/login', {
+      method: 'post',
+      data: {
+        user: {
+          email: 'qq@qq.qq',
+          password: '123456'
+        }
+      }
+    })
+      .then(res => console.log('success', res))
+      .catch(err => console.error('error', err))
+      .finally(() => setIsSubmitting(false));
+
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setIsSubmitting(true);
+    console.log(email, password);
+  };
+
   return (
     <div className='auth-page'>
       <div className='container page'>
@@ -13,17 +46,32 @@ const Authentication = () => {
               <Link to='register'>Need An Account?</Link>
             </p>
 
-            <form action="">
+            <form onSubmit={handleSubmit}>
               <fieldset>
                 <fieldset className='form-group'>
-                  <input type="email" className='form-control form-control-lg' placeholder='Email'/>
+                  <input
+                    type="email"
+                    className='form-control form-control-lg'
+                    placeholder='Email'
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}/>
                 </fieldset>
 
                 <fieldset className='form-group'>
-                  <input type="password" className='form-control form-control-lg' placeholder='Password'/>
+                  <input
+                    type="password"
+                    className='form-control form-control-lg'
+                    placeholder='Password'
+                    autoComplete='on'
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}/>
                 </fieldset>
 
-                <button type='submit' className='btn btn-lg btn-primary pull-xs-right'>Sing In</button>
+                <button
+                  type='submit'
+                  className='btn btn-lg btn-primary pull-xs-right' disabled={isSubmitting}>
+                  Sing In
+                </button>
               </fieldset>
             </form>
           </div>
