31c7fcf5dcaf6a37201c73337b4fc648b542d925
YuriAnisimov27 <yurianisimov27@gmail.com>
2021-03-04 19:53:36 +0300

    feat: impl component Feed

---
+++ medium-clone/src/components/feed.js
@@ -0,0 +1,35 @@
+import {Link} from 'react-router-dom';
+
+const Feed = ({articles}) => {
+  return (
+    <div>
+      {articles.map((article, idx) => (
+        <div className='article-preview' key={idx}>
+          <div className='article-meta'>
+            <Link to={`/profiles/${article.author.username}`}>
+              <img src={article.author.image} alt="author"/>
+            </Link>
+            <div className='info'>
+              <Link className='author' to={`/profiles/${article.author.username}`}>{article.author.username}</Link>
+              <span className='date'>{article.createdAt}</span>
+            </div>
+          </div>
+          <Link className='preview-link' to={`/articles/${article.slug}`}>
+            <h2>{article.title}</h2>
+            <p>{article.description}</p>
+            <span>Read more...</span>
+            <ul className='tag-list'>
+              {article.tagList.map(tag => (
+                <li key={tag} className='tag-default tag-pill tag-outline'>
+                  {tag}
+                </li>
+              ))}
+            </ul>
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Feed;
--- medium-clone/src/pages/globalFeed/index.js
+++ medium-clone/src/pages/globalFeed/index.js
@@ -1,6 +1,38 @@
+import {useEffect} from 'react';
+import useFetch from '../../hooks/useFetch';
+import Feed from '../../components/feed';
+
+
 const GlobalFeed = () => {
+  const apiUrl = `/articles?limit=10&offset=0`;
+  const [{response, isLoading, error}, doFetch] = useFetch(apiUrl);
+
+  useEffect(() => {
+    doFetch();
+  }, [doFetch]);
+
   return (
-    <div>GlobalFeed</div>
+    <div className='home-page'>
+      <div className='banner'>
+        <div className='container'>
+          <h1>Medium</h1>
+          <p>A place to share knowledge</p>
+        </div>
+      </div>
+
+      <div className='container page'>
+        <div className='row'>
+          <div className='col-md-9'>
+            {isLoading && <p>Loading</p>}
+            {error && <p>Error!</p>}
+            {!isLoading && response && <Feed articles={response.articles}/>}
+          </div>
+          <div className='col-md-3'>
+            Popular Tags
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
