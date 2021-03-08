4c45000df4f48c653cfa21081b47bff4ea150b48
YuriAnisimov27 <yurianisimov27@gmail.com>
2021-03-08 18:36:03 +0300

    feat: impl YourFeed

--- medium-clone/src/components/feedToggler.js
+++ medium-clone/src/components/feedToggler.js
@@ -1,12 +1,17 @@
+import {useContext} from 'react';
 import {NavLink} from 'react-router-dom';
+import {CurrentUserContext} from '../context/currentUser';
+
 
 const FeedToggler = ({tagName}) => {
+  const [currentUserState] = useContext(CurrentUserContext);
+
   return (
     <div className='feed-toggle'>
       <ul className='nav nav-pills outline-active'>
-        <li className='nav-item'>
+        {currentUserState.isLoggedIn && (<li className='nav-item'>
           <NavLink to='/feed' className='nav-link'>Your Feed</NavLink>
-        </li>
+        </li>)}
         <li className='nav-item'>
           <NavLink to='/' className='nav-link' exact>Global Feed</NavLink>
         </li>
