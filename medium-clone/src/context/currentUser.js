840c8a7346cdcd27bd93184176a9fea40fe52dc2
YuriAnisimov27 <yurianisimov27@gmail.com>
2021-03-10 11:12:24 +0300

    feat: add reducer

--- medium-clone/src/context/currentUser.js
+++ medium-clone/src/context/currentUser.js
@@ -1,7 +1,39 @@
 import {createContext, useState} from 'react';
 
 
-export const CurrentUserContext = createContext([{}, () => {}]);
+const initialState = {
+  isLoading: false,
+  isLoggedIn: null,
+  currentUser: null
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'LOADING':
+      return {
+        ...state,
+        isLoading: true
+      };
+
+    case 'SET_AUTHORIZED':
+      return {
+        ...state,
+        isLoggedIn: true,
+        isLoading: false,
+        currentUser: action.payload
+      };
+    case 'SET_UNAUTHORIZED':
+      return {
+        ...state,
+        isLoggedIn: false
+      };
+    default:
+      return state;
+  }
+};
+
+export const CurrentUserContext = createContext([{}, () => {
+}]);
 export const CurrentUserProvider = ({children}) => {
   const [state, setState] = useState({
     isLoading: false,
