13b20ae01766326ddc1c686d278ab688e00e20e3
YuriAnisimov27 <yurianisimov27@gmail.com>
2021-03-20 19:21:10 +0300

    feat: implement userArticles logic

--- medium-clone/src/pages/userProfile/components/userArticles.js
+++ medium-clone/src/pages/userProfile/components/userArticles.js
@@ -1,4 +1,28 @@
+import {useEffect} from 'react';
+import {getPaginator, limit} from '../../../utils';
+import {stringify} from 'query-string';
+import useFetch from '../../../hooks/useFetch';
+
+
+const getApiUrl = ({username, offset, isFavorites}) => {
+  const params = isFavorites
+    ? {limit, offset, favorited: username}
+    : {limit, offset, author: username};
+
+  return `/articles?${stringify(params)}`;
+};
+
 const UserArticles = ({username, location, isFavorites}) => {
+  const {offset, currentPage} = getPaginator(location.search);
+  const apiUrl = getApiUrl({username, offset, isFavorites});
+  const [{response, error, isLoading}, doFetch] = useFetch(apiUrl);
+
+  console.log('response', response);
+
+  useEffect(() => {
+    doFetch()
+  }, [doFetch]);
+
   return (
     <div>User articles</div>
   );
